aef81685d5644e5d3a2d9ee53448c838f0006159
khjj07 <khjj07@gmail.com>
2024-06-16 18:24:37 +0900

    안개 테스트중

--- Assets/TicTocGuardians/Scripts/Game/LevelObjects/FogLevelObject.cs
+++ Assets/TicTocGuardians/Scripts/Game/LevelObjects/FogLevelObject.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 namespace TicTocGuardians.Scripts.Game.LevelObjects
 {
@@ -6,7 +7,22 @@ namespace TicTocGuardians.Scripts.Game.LevelObjects
     {
         public class FogLevelObject : LevelObject
         {
+            public bool simulate = true;
+        }
 
+        [CustomEditor(typeof(FogLevelObject))]
+        public class FogLevelObjectInspector : Editor
+        {
+            
+            public void OnValidate()
+            {
+                var levelObject = (FogLevelObject)target;
+                if (levelObject.simulate)
+                {
+                    var ps = levelObject.GetComponent<ParticleSystem>();
+                    ps.Simulate(1,true,true); //set value and play straightly
+                }
+            }
         }
     }
 }
